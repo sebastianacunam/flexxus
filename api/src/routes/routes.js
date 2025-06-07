2e530ca7eca5c75f64c67106c97c0877ca29c737
sebastianacunam <sebasjam96@gmail.com>
2025-06-07 15:11:20 -0300

    conexion a base de datos, servidor levantado y primera ruta generada

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getAllArticles, createArticle } = require('../controllers/articleControllers');
 
 // Definición de rutas
-router.get('/getAllArticles', getAllArticles);
+router.get('/ ', getAllArticles);
 // router.post('/createArticle', createArticle);
 
 module.exports = router; // <- esto es crucial
